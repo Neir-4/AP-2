589a434ddc9f97bf0019055faf329b0a7650fb34
YehezkielStg <140903242+Neir-4@users.noreply.github.com>
2025-03-07 15:22:31 +0700

    Pertemuan 1 : Pengenalan dan Operasi Input/Output

---
+++ Pertemuan 1/belajar.cpp
@@ -0,0 +1,36 @@
+#include <iostream> //header untuk C++
+#include <conio.h> //header untuk fungsi getch() dan getche()
+using namespace std;  
+
+int main(){
+    string nama;
+    char kom, jeniskelamin;
+    int nim;
+    float ip;
+
+    cout<<"Hello World"<<endl;
+
+    cout<<"Masukkan Nama Anda : "; //untuk keluaran
+    getline(cin,nama); // agar karakter spasi bisa dibaca
+
+    cout<<"Masukkan NIM Anda : "; 
+    cin>>nim; //untuk masukan
+
+    cout<<"Masukkan KOM Anda : "; 
+    cin>>kom;
+    
+    cout<<"Masukkan IP Anda : "; 
+    cin>>ip;
+
+    cout<<"Masukkan Jenis Kelamin (L/P) : ";
+    jeniskelamin = getche();
+
+    cout<<"\n"<<"Nama Anda : "<<nama<<endl;
+    cout<<"NIM Anda : "<<nim<<endl;
+    cout<<"KOM Anda : "<<kom<<endl;
+    cout<<"IP Anda : "<<ip<<endl;
+    cout<<"Jenis Kelamin Anda : "; putchar(jeniskelamin);
+
+    getch(); //karakter yang diketik tidak di tampilkan di layar
+    /*Comment multiline*/
+}
