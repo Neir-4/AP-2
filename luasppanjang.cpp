ce5ea6dd063650ef0ae52409153eae541868c382
YehezkielStg <140903242+Neir-4@users.noreply.github.com>
2025-03-14 15:27:35 +0700

    Pertemuan 2 : Penggunaan Operator

---
+++ luasppanjang.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+main() {
+    float luas , p , l;
+
+    cout<<"Masukkann Panjang : "; cin >> p;
+    cout<<"Masukkan Lebar : "; cin>>l;
+    luas = p * l;
+    cout<< "Luas : "<<luas<<endl;
+
+}
