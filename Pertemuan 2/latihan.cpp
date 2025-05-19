2a0e93a0f0d15d76214fe4816a041fc6a422513f
YehezkielStg <140903242+Neir-4@users.noreply.github.com>
2025-05-19 08:33:25 +0700

    Pertemuan 2 : Penggunaan Operator

--- Pertemuan 2/latihan.cpp
+++ Pertemuan 2/latihan.cpp
@@ -1,14 +1,16 @@
+//header cpp
 #include <iostream>
 using namespace std;
 
 main() {
+    //inisialisasi variabel
     int jari2 , volume , lp;
-    const float pi = 3.14;
+    const float pi = 3.14; //const merupakan nilai tetap yang tidak bisa diganti
 
     cout << "Masukkan Jari jari lingkaran : "; cin >> jari2;
-    volume = float(4) / 3 * pi * jari2 * jari2 * jari2;
+    volume = float(4) / 3 * pi * jari2 * jari2 * jari2; //float(4) gunanya untuk merubah nilai int menjadi float
     lp = 4 * pi * jari2 * jari2;
-    cout<<"Volume : "<<volume << endl;
-    cout<<"Luas Permukaan : "<<lp << endl;
+    cout<<"Volume : "<<volume << endl; //menampilkan volume
+    cout<<"Luas Permukaan : "<<lp << endl; //menampilkan luas permukaan
 
 }
