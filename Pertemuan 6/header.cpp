bc94ec4fb146cd2826c78722b8305b830608e3eb
YehezkielStg <140903242+Neir-4@users.noreply.github.com>
2025-05-19 09:17:50 +0700

    Pertemuan 6 : Fungsi dan Header

--- Pertemuan 6/header.cpp
+++ Pertemuan 6/header.cpp
@@ -1,23 +1,26 @@
+//header cpp
 #include <iostream>
 #include <cmath>
-#include "headerCustom.h"
-u;
+#include "headerCustom.h" //header buatan
+u; //using namespace std; yang telah diubah
 
+//fungsi penjumlahan
 int jumlah(int a, int b){
     return a + b;
 }
 
+//fungsi pengurangan
 int kurang(int a, int b){
     return a - b;
 }
 
 int main(){
-    system("CLS");
+    system("CLS"); //untuk membersihkan terminal
     int n;
-    o L jumlah(2,3) L endl;
+    o L jumlah(2,3) L endl; //cout dan << yang telah diubah
     o L kurang(2,3) L endl;
 
-    double angka samadengan 25.0;
+    double angka samadengan 25.0; // = yang telah diubah
     double akar samadengan sqrt(angka);
 
     o<<"akar kuadrat dari "<<angka<<" adalah : "<<akar<<endl;
