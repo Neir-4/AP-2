c85cf2f25e7241c50e4781083b075d26f4ccbd1e
YehezkielStg <140903242+Neir-4@users.noreply.github.com>
2025-05-19 09:09:24 +0700

    Pertemuan 4 : Perulangan

--- Pertemuan 4/latihan3.cpp
+++ Pertemuan 4/latihan3.cpp
@@ -1,15 +1,17 @@
+//header cpp
 #include <iostream>
 using namespace std;
 
 int main() {
+    //inisialisasi variabel
     string sandi, tulis;
     int i,j;
-    system("CLS");
-    cout<<"Masukkan Kata sandi : "; cin>>sandi;
+    system("CLS"); //membersihkan terminal
+    cout<<"Masukkan Kata sandi : "; cin>>sandi; 
     cout<<"Masukkan Kata Yang ditulis : "; cin>>tulis;
-    for (i=0; i<sandi.length(); i++) {
-        for (j=0; j<sandi.length();j++){
-            cout<<sandi.length();
+    for (i=0; i<sandi.length(); i++) { //perulangan ketika i yang bernilai 0 lebih kecil nilainya dari panjang karakter sandi , nilai i akan bertambah 1
+        for (j=0; j<sandi.length();j++){ //perulangan ketika j yang bernilai 0 lebih kecil nilainya dari panjang karakter sandi , nilai j akan bertambah 1
+            cout<<sandi.length();  //menampilkan sandi 
         }
     } return 0;
     
