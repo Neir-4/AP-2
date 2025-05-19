5e1d85cbc0bc6d3987d7142f837a6f8be1ed3777
YehezkielStg <140903242+Neir-4@users.noreply.github.com>
2025-05-19 09:37:30 +0700

    Pertemuan 8 : Struct & Class

--- Pertemuan 8/classLevel.cpp
+++ Pertemuan 8/classLevel.cpp
@@ -1,23 +1,27 @@
+//header cpp
 #include <iostream>
 using namespace std;
 
+//pembuatan class contohAkses
 class contohAkses {
+    //bagian private
     private: 
         int privateVar;
-
+    //bagian protected
     protected:
         int protectedVar;
-
+    //bagian public
     public: 
         int publicVar;
         //Constructor 
+        //fungsi contohAkses
         contohAkses(){
             privateVar = 1;
             protectedVar = 2;
             publicVar = 3;
         }
 
-
+//prosedur tampilkanSemua untuk menampilkan nilai dari setiap bagian di class
     void tampilkanSemua(){
         cout<<"Akses dari dalam class : "<<endl;
         cout<<privateVar<<endl;
@@ -29,21 +33,22 @@ class contohAkses {
 
 //Class turunan
 class Turunan: public contohAkses{
+    //membuat bagian protected dari class utama menjadi diturunkan kedalam class yang public
     public:
     void aksesProtected(){
         cout<<"Akses publicVar "<<publicVar<<endl;
         cout<<"Akses protectedVar "<<protectedVar<<endl;
-        // cout<<"Akses privateVar "<<privateVar<<endl; error
+        // cout<<"Akses privateVar "<<privateVar<<endl; error //meskipun telah diturunkan bagian private tetap tidak bisa diakses diluar dari class utama
     }
 };
 
 int main(){
-    contohAkses obj;
-    obj.tampilkanSemua();
+    contohAkses obj; //inisialisasi variabel obj dengan tipe data class contohAkses
+    obj.tampilkanSemua(); //memanggil prosedur tampilkanSemua
     cout<<"Akses dari luar Class : "<<endl;
-    cout<<obj.publicVar<<endl;
-    // cout<<obj.protectedVar<<endl; //error
-    // cout<<obj.privateVar<<endl; // error
-    Turunan tur;
-    tur.aksesProtected();
+    cout<<obj.publicVar<<endl; //memanggil nilai dari bagian public (sukses)
+    // cout<<obj.protectedVar<<endl; //memanggil nilai dari bagian protected tanpa class turunan (error)
+    // cout<<obj.privateVar<<endl; //memanggil nilai dari bagian private tanpa class turunan (error)
+    Turunan tur; //inisialisasi variabel tur dengan tipe data class turunan Turunan
+    tur.aksesProtected(); //memanggil prosedur aksesProtected
 }
