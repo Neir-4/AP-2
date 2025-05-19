f71648fe40756e3789d2f9cc4fc8dac4cd1e9fd7
YehezkielStg <140903242+Neir-4@users.noreply.github.com>
2025-05-19 09:13:23 +0700

    Pertemuan 6 : Fungsi dan Header

--- Pertemuan 6/belajarFungsi.cpp
+++ Pertemuan 6/belajarFungsi.cpp
@@ -1,12 +1,14 @@
+//header cpp
 #include <iostream>
 using namespace std;
 
+//void untuk prosedur yang tidak memiliki nilai balikan
 void sapa(string nama){
     cout<<"Halo "<<nama<<" ! Selamat datang di AP2!"<<endl;
 }
 
 int main(){
     string namapengguna = "Kiel";
-    sapa(namapengguna);
+    sapa(namapengguna);    //memanggil prosedur sapa dengan nilai parameter nya dari namapengguna
 
 }
