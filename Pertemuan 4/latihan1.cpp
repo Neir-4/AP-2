4e54d36b1044157d6b502b823f1c0e8dac467fd1
YehezkielStg <140903242+Neir-4@users.noreply.github.com>
2025-05-19 09:02:20 +0700

    Pertemuan 4 : Perulangan

--- Pertemuan 4/latihan1.cpp
+++ Pertemuan 4/latihan1.cpp
@@ -1,14 +1,16 @@
+//header cpp
 #include <iostream>
 using namespace std;
 
 int main(){
+    //inisialisasi variabel
     string kalimat;
     int i;
-    system("CLS");
-    cout<<"Masukkan Kalimat : "; getline(cin,kalimat);
-    for(i=0; i<kalimat.length(); i++ ){
-        kalimat[i] = toupper(kalimat[i]);
+    system("CLS"); //untuk membersihkan terminal
+    cout<<"Masukkan Kalimat : "; getline(cin,kalimat); //getline untuk mendapatkan nama lengkap dengan spasi
+    for(i=0; i<kalimat.length(); i++ ){    //memulai perulangan ketika nilai i lebih kecil dari panjang karakter kalimat , nilai awal i adalah 0 dan akan bertambah 1 seiring perulangan
+        kalimat[i] = toupper(kalimat[i]);    //index atau karakter dari kalimat akan diubah kedalam huruf kapital
     }
-    cout<<"kalimat dalam huruf kapital : "<<kalimat<<endl;
+    cout<<"kalimat dalam huruf kapital : "<<kalimat<<endl; //menampilkan kalimat
     return 0;
 }
