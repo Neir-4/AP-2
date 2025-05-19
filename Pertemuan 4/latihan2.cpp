919e83f64a1039ab8dddb4da410b0f2531763522
YehezkielStg <140903242+Neir-4@users.noreply.github.com>
2025-05-19 09:05:35 +0700

    Pertemuan 4 : Perulangan

--- Pertemuan 4/latihan2.cpp
+++ Pertemuan 4/latihan2.cpp
@@ -1,15 +1,17 @@
+//header cpp
 #include <iostream>
 using namespace std;
 
 int main() {
+    //inisialisasi variabel
     string kalimat;
     int i;
-    system("CLS");
-    cout<<"Masukkan Kalimat : "; getline(cin,kalimat);
-    for (i=0; i<kalimat.length(); i++){
-        if (kalimat[i]=='a'||kalimat[i]=='e'||kalimat[i]=='i'||kalimat[i]=='o'||kalimat[i]=='u'){
-        kalimat[i] = toupper(kalimat[i]);
+    system("CLS"); //untuk membersihkan terminal
+    cout<<"Masukkan Kalimat : "; getline(cin,kalimat); //menerima input kalimat bahkan dengan spasi
+    for (i=0; i<kalimat.length(); i++){ //perulangan ketika i yang bernilai 0 lebih kecil nilai nya dari panjang karakter kalimat, i akan bertambah 1
+        if (kalimat[i]=='a'||kalimat[i]=='e'||kalimat[i]=='i'||kalimat[i]=='o'||kalimat[i]=='u'){ //operasi kondisi jika index atau karakter dari kalimat huruf a , e , i , o , dan u
+        kalimat[i] = toupper(kalimat[i]); //huruf dari kalimat akan diubah ke huruf kapital
         }
-    } cout<<kalimat<<endl;
+    } cout<<kalimat<<endl; //menampilkan kalimat
     return 0;
 }
