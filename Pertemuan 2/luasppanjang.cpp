625c05c0d382fa1077e5ebe06003b3b2a3c55863
YehezkielStg <140903242+Neir-4@users.noreply.github.com>
2025-05-19 08:34:24 +0700

    Pertemuan 2 : Penggunaan Operator

--- Pertemuan 2/luasppanjang.cpp
+++ Pertemuan 2/luasppanjang.cpp
@@ -1,12 +1,14 @@
+//header cpp
 #include <iostream>
 using namespace std;
 
 main() {
+    //inisialisasi variabel
     float luas , p , l;
 
-    cout<<"Masukkann Panjang : "; cin >> p;
+    cout<<"Masukkann Panjang : "; cin >> p; //meminta masukkan dan disimpan ke variabel p
     cout<<"Masukkan Lebar : "; cin>>l;
-    luas = p * l;
-    cout<< "Luas : "<<luas<<endl;
+    luas = p * l; //operasi perkalian 
+    cout<< "Luas : "<<luas<<endl;   //menampilkan luas
 
 }
