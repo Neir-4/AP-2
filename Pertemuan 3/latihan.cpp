c1207e912ee0d12814c1e7c76d0e69ceee85b254
YehezkielStg <140903242+Neir-4@users.noreply.github.com>
2025-05-19 08:40:34 +0700

    Pertemuan 3 : Operasi Kondisi

--- Pertemuan 3/latihan.cpp
+++ Pertemuan 3/latihan.cpp
@@ -1,16 +1,18 @@
+//header cpp
 #include <iostream>
 using namespace std;
 
 int main() {
+    //inisialisasi variabel
     int bil;
-    cout<<"Masukkan bilangan : "; cin>>bil;
-    if (bil % 5 == 0 && bil % 10 == 0) {
+    cout<<"Masukkan bilangan : "; cin>>bil; //mengeluarkan dan meminta masukan untuk nilai variabel bil
+    if (bil % 5 == 0 && bil % 10 == 0) { //jika bil sisa bagi 5 adalah 0 dan bil sisa bagi 10 adalah 10 
         cout<<"Bilangan "<< bil << " adalah kelipatan 5 dan 10"<< endl;
-    } else if (bil % 5 == 0 ){
+    } else if (bil % 5 == 0 ){ //jika bil sisa bagi 5 adalah 0
         cout<<"Bilangan "<< bil << " adalah kelipatan 5"<< endl;
-    } else if (bil % 10 == 0 ) {
+    } else if (bil % 10 == 0 ) { //jika bil sisa bagi 10 adalah 0
         cout<<"Bilangan "<< bil << " adalah kelipatan 10"<< endl;
-    } else {
+    } else {    //selain dari kondisi sebelumnya
         cout<<"Bilangan "<<bil << " Bukan kelipatan 5 atau 10"<<endl;
     }
 }
