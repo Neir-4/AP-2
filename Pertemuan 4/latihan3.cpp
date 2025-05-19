0c061695f0cfd754efe7d1a436f060adf9278339
YehezkielStg <140903242+Neir-4@users.noreply.github.com>
2025-05-19 08:14:07 +0700

    Pertemuan 4 : Perulangan

---
+++ Pertemuan 4/latihan3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string sandi, tulis;
+    int i,j;
+    system("CLS");
+    cout<<"Masukkan Kata sandi : "; cin>>sandi;
+    cout<<"Masukkan Kata Yang ditulis : "; cin>>tulis;
+    for (i=0; i<sandi.length(); i++) {
+        for (j=0; j<sandi.length();j++){
+            cout<<sandi.length();
+        }
+    } return 0;
+    
+}
