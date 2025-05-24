0a0366fb03793a8427dbc2166bc431fff3779b26
YehezkielStg <140903242+Neir-4@users.noreply.github.com>
2025-05-25 00:03:02 +0700

    Update vektor.cpp

--- Pertemuan 5/vektor.cpp
+++ Pertemuan 5/vektor.cpp
@@ -1,12 +1,13 @@
+//header cpp
 #include <iostream>
 #include <vector>
 using namespace std;
 
 int main() {
-    //vector declaration & initialization
+    //vector declaration & initialization | Deklarasi vector
     vector<string> nama_karyawan = {"Alya", "Kiel", "Rifki", "Farhan", "Dafa"};
 
-    //ascending element of vector
+    //ascending element of vector | Nilai menaik di vector
     // for (int i = 0 ; i < nama_karyawan.size(); i++){
     //     cout<<nama_karyawan[i]<<endl;
     // }
@@ -15,13 +16,13 @@ int main() {
     //     cout<<karyawan <<endl;
     // }
 
-    //Add data to vector
+    //Add data to vector | Menambahkan data ke urutan belakang 
     nama_karyawan.push_back("Imam");
     // for (int i = 0 ; i < nama_karyawan.size(); i++){
     //     cout<<nama_karyawan[i]<<endl;
     // }    
 
-    // Delete data from vector
+    // Delete data from vector | Menghapus data dari vector
     nama_karyawan.pop_back();
     nama_karyawan.erase(nama_karyawan.begin() + 3);
     for (int i = 0 ; i < nama_karyawan.size(); i++){
